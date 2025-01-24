64b69ed3318a8f9ad345fdcd563f93d15dc662df
mbledkowski <pub@mble.dk>
2025-01-24 19:54:35 +0100

    First working version

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+rm -r ./build
+mkdir -p build/mods build/config build/resourcepacks
+cp -r ./repos/additive/versions/fabric/1.21.4/mods/* ./build/mods/
+cp -r ./repos/additive/versions/fabric/1.21.4/config/* ./build/config/
+cp -r ./repos/additive/versions/fabric/1.21.4/resourcepacks/* ./build/resourcepacks/
+cp -rf ./repos/simply_optimized/so\ 1.21.4\ building/mods/* ./build/mods/
+cp -rf ./repos/simply_optimized/so\ 1.21.4\ building/config/* ./build/config/
+symlinks=$(find -L ./mods -xtype l)
+for symlink in $symlinks
+do
+ln --force --symbolic "$(readlink --canonicalize "$symlink")" "$symlink"
+done
+cp -rf ./mods/* ./build/mods/
+for symlink in $symlinks
+do
+ln --force --symbolic "$(readlink --canonicalize ./build/"$symlink")" ./build/"$symlink"
+done
+cp ./pack.toml ./index.toml ./build/
+cd ./build
+packwiz refresh
+packwiz modrinth export
